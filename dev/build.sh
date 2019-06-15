3a85de4cc8616fd913c18eef480ba61bc2b44d00
Michael McClintock <mrmcc3@gmail.com>
2019-06-15 22:26:51 +1000

    rebuild examples

--- dev/build.sh
+++ dev/build.sh
@@ -8,13 +8,13 @@ rm -rf docs
 clojure -Adev -m figwheel.main -O advanced -bo dev
 mkdir -p docs/js
 cp resources/public/js/*.js docs/js
-rm -rf resources/public/js
+rm -rf resources/public/js/*
 
 # build css
 mkdir -p docs/css
 cleancss -O2 resources/public/css/examples.css -o docs/css/examples.css
 postcss docs/css/examples.css -u autoprefixer -r --no-map
-rm -rf resources/public/css
+rm -rf resources/public/css/*
 
 # install the rest
 cp -rf resources/public/* docs
